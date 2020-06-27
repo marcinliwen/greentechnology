7551a176f9bbc8476ba84d4f7e19cc12b1a2e766
marcinliwen <liwen.marcin@gmail.com>
2020-06-27 13:25:56 +0200

    new icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#2b2b2b`,
         theme_color: `#2b2b2b`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gtech-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
