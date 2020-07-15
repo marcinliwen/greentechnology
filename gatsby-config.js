ffaaf94dc5e1048a88c5e16b7a04a020a0178bd8
marcinliwen <liwen.marcin@gmail.com>
2020-07-15 15:05:38 +0200

    gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,17 @@ module.exports = {
         trackingId: "UA-91684421-2",
       },
     },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-91684421-2`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
