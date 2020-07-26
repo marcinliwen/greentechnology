bb421e885bcd4f20b0c594af7020dee55e0c0c5b
liwenmarcin <marcin.liwen@sohoshop.pl>
2020-07-27 01:02:58 +0200

    reaction on click

--- src/components/Jaktodziala.js
+++ src/components/Jaktodziala.js
@@ -25,28 +25,48 @@ const Jaktodziala = () =>{
     }));
     const classes = useStyles();
 
+    function mappingHandle(e){
+        var index = e.target.getAttribute('data_index');
+        const mapItemList = document.querySelectorAll('.jtd_mapping_item')
+        const mapItemDescList = document.querySelectorAll('.jtd_mapping_desc_item');
+
+        for(let i = 0; i < mapItemList.length; i++){
+            if(mapItemList[i].classList.contains('this')){
+                mapItemList[i].classList.remove('this')
+            }
+        }
+    
+        for(let i = 0; i < mapItemDescList.length; i++){
+            if(mapItemDescList[i].classList.contains('this')){
+                mapItemDescList[i].classList.remove('this')
+            }
+        }
+        document.querySelector('.jtd_mapping_item[data_index="'+index+'"').classList.add('this');
+        document.querySelector('.jtd_mapping_desc_item[data_index="'+index+'"').classList.add('this');
+
+    }
     return(
-    <div class="section_content jtd_content">
+    <div className="section_content jtd_content">
         <div className="jtd_img">
           <img src={jaktodziala_img} alt="jak działa fotowoltaika"/>
         </div>
         <div className="jdt_mapping_container">
           <div className="jtd_mapping">
-            <div className="jtd_mapping_item">1</div>
-            <div className="jtd_mapping_item">2</div>
-            <div className="jtd_mapping_item">3</div>
-            <div className="jtd_mapping_item">4</div>
-            <div className="jtd_mapping_item">5</div>
-            <div className="jtd_mapping_item">6</div>
+            <div className="jtd_mapping_item" onClick={mappingHandle} data_index="1">1</div>
+            <div className="jtd_mapping_item" onClick={mappingHandle} data_index="2">2</div>
+            <div className="jtd_mapping_item" onClick={mappingHandle} data_index="3">3</div>
+            <div className="jtd_mapping_item" onClick={mappingHandle} data_index="4">4</div>
+            <div className="jtd_mapping_item" onClick={mappingHandle} data_index="5">5</div>
+            <div className="jtd_mapping_item" onClick={mappingHandle} data_index="6">6</div>
           </div>
-            <ol className="jtd_mapping_description">
-                <li className="jtd_mapping_desc_item">panele fotowoltaiczne</li>
-                <li className="jtd_mapping_desc_item">inverter (falownik)</li>
-                <li className="jtd_mapping_desc_item">dwukierunkowy licznik energii elektrycznej</li>
-                <li className="jtd_mapping_desc_item">bilansowanie energii z siecią</li>
-                <li className="jtd_mapping_desc_item">linia zakładu energetycznego</li>
-                <li className="jtd_mapping_desc_item">odbiorniki</li>
-            </ol>  
+            <div className="jtd_mapping_description">
+                <div className="jtd_mapping_desc_item" onClick={mappingHandle} data_index="1">1. panele fotowoltaiczne</div>
+                <div className="jtd_mapping_desc_item" onClick={mappingHandle} data_index="2">2. inverter (falownik)</div>
+                <div className="jtd_mapping_desc_item" onClick={mappingHandle} data_index="3">3. dwukierunkowy licznik energii elektrycznej</div>
+                <div className="jtd_mapping_desc_item" onClick={mappingHandle} data_index="4">4. bilansowanie energii z siecią</div>
+                <div className="jtd_mapping_desc_item" onClick={mappingHandle} data_index="5">5. linia zakładu energetycznego</div>
+                <div className="jtd_mapping_desc_item" onClick={mappingHandle} data_index="6">6. odbiorniki</div>
+            </div>  
             <div className="jdt_mapping_link">
                     <Button variant="outlined" className={classes.root, classes.outlined}>
                         <Link className="navbar-item" 
