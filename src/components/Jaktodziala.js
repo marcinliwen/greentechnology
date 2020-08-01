190300411bc6a14a3b7a8dfd0632089bcd5e6fb4
marcinliwen <liwen.marcin@gmail.com>
2020-08-01 22:41:01 +0200

    onmouseenter style

--- src/components/Jaktodziala.js
+++ src/components/Jaktodziala.js
@@ -45,6 +45,22 @@ const Jaktodziala = () =>{
         document.querySelector('.jtd_mapping_desc_item[data_index="'+index+'"').classList.add('this');
 
     }
+    function mappingLeave(){
+        const mapItemList = document.querySelectorAll('.jtd_mapping_item')
+        const mapItemDescList = document.querySelectorAll('.jtd_mapping_desc_item');
+
+        for(let i = 0; i < mapItemList.length; i++){
+            if(mapItemList[i].classList.contains('this')){
+                mapItemList[i].classList.remove('this')
+            }
+        }
+    
+        for(let i = 0; i < mapItemDescList.length; i++){
+            if(mapItemDescList[i].classList.contains('this')){
+                mapItemDescList[i].classList.remove('this')
+            }
+        }
+    }
     return(
     <div className="section_content jtd_content">
         <div className="jtd_img">
@@ -52,20 +68,20 @@ const Jaktodziala = () =>{
         </div>
         <div className="jdt_mapping_container">
           <div className="jtd_mapping">
-            <div className="jtd_mapping_item" onClick={mappingHandle} data_index="1">1</div>
-            <div className="jtd_mapping_item" onClick={mappingHandle} data_index="2">2</div>
-            <div className="jtd_mapping_item" onClick={mappingHandle} data_index="3">3</div>
-            <div className="jtd_mapping_item" onClick={mappingHandle} data_index="4">4</div>
-            <div className="jtd_mapping_item" onClick={mappingHandle} data_index="5">5</div>
-            <div className="jtd_mapping_item" onClick={mappingHandle} data_index="6">6</div>
+            <div className="jtd_mapping_item" onMouseEnter={mappingHandle}  onMouseLeave={mappingLeave} data_index="1">1</div>
+            <div className="jtd_mapping_item" onMouseEnter={mappingHandle}  onMouseLeave={mappingLeave} data_index="2">2</div>
+            <div className="jtd_mapping_item" onMouseEnter={mappingHandle}  onMouseLeave={mappingLeave} data_index="3">3</div>
+            <div className="jtd_mapping_item" onMouseEnter={mappingHandle}  onMouseLeave={mappingLeave} data_index="4">4</div>
+            <div className="jtd_mapping_item" onMouseEnter={mappingHandle}  onMouseLeave={mappingLeave} data_index="5">5</div>
+            <div className="jtd_mapping_item" onMouseEnter={mappingHandle}  onMouseLeave={mappingLeave} data_index="6">6</div>
           </div>
             <div className="jtd_mapping_description">
-                <div className="jtd_mapping_desc_item" onClick={mappingHandle} data_index="1">1. panele fotowoltaiczne</div>
-                <div className="jtd_mapping_desc_item" onClick={mappingHandle} data_index="2">2. inverter (falownik)</div>
-                <div className="jtd_mapping_desc_item" onClick={mappingHandle} data_index="3">3. dwukierunkowy licznik energii elektrycznej</div>
-                <div className="jtd_mapping_desc_item" onClick={mappingHandle} data_index="4">4. bilansowanie energii z siecią</div>
-                <div className="jtd_mapping_desc_item" onClick={mappingHandle} data_index="5">5. linia zakładu energetycznego</div>
-                <div className="jtd_mapping_desc_item" onClick={mappingHandle} data_index="6">6. odbiorniki</div>
+                <div className="jtd_mapping_desc_item" onMouseEnter={mappingHandle}  onMouseLeave={mappingLeave} data_index="1">1. panele fotowoltaiczne</div>
+                <div className="jtd_mapping_desc_item" onMouseEnter={mappingHandle}  onMouseLeave={mappingLeave} data_index="2">2. inverter (falownik)</div>
+                <div className="jtd_mapping_desc_item" onMouseEnter={mappingHandle}  onMouseLeave={mappingLeave} data_index="3">3. dwukierunkowy licznik energii elektrycznej</div>
+                <div className="jtd_mapping_desc_item" onMouseEnter={mappingHandle}  onMouseLeave={mappingLeave} data_index="4">4. bilansowanie energii z siecią</div>
+                <div className="jtd_mapping_desc_item" onMouseEnter={mappingHandle}  onMouseLeave={mappingLeave} data_index="5">5. linia zakładu energetycznego</div>
+                <div className="jtd_mapping_desc_item" onMouseEnter={mappingHandle}  onMouseLeave={mappingLeave} data_index="6">6. odbiorniki</div>
             </div>  
             <div className="jdt_mapping_link">
                     <Button variant="outlined" className={classes.root, classes.outlined}>
